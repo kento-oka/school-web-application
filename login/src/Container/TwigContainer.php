c74219fcbb1b7183622a6d772182a0c94a0f2483
kento-oka <kento-oka@kentoka.com>
2018-10-03 13:02:36 +0900

    コンテナのバージョンが上がった

---
+++ login/src/Container/TwigContainer.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Container;
+
+use Fratily\Container\Builder\AbstractContainer;
+use Fratily\Container\Builder\ContainerBuilderInterface;
+
+class TwigContainer extends AbstractContainer{
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function build(ContainerBuilderInterface $builder, array $options){
+        $builder->add(
+            "app.twig.loader",
+            new \Twig_Loader_Filesystem([__DIR__ . "/../../template"]),
+            ["twig.loader"]
+        );
+    }
+}
